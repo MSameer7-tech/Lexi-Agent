c398b94f751e8d98dd756f8aac12a9114de1648c
MSameer7-tech <8109496805b@gmail.com>
2026-09-25 01:43:05 +0530

    chore: final production repository cleanup

--- ocr.js
+++
@@ -1,9 +0,0 @@
-import { createWorker } from 'tesseract.js';
-import fs from 'fs';
-
-(async () => {
-  const worker = await createWorker('eng');
-  const ret = await worker.recognize('/Users/sameer/.gemini/antigravity/brain/95a06a5c-a54f-4a9d-8fb7-8cc94eb44fca/.user_uploaded/media_1790009371550.png');
-  console.log(ret.data.text);
-  await worker.terminate();
-})();
